742ada962d05553cfd56cdf61443084bf6dbd773
RJP0426 <31867093+RJP0426@users.noreply.github.com>
2017-10-30 18:30:46 -0500

    Add files via upload

---
+++ MyVideo/MyVideo/videos.swift
@@ -0,0 +1,13 @@
+//
+//  videos.swift
+//  MyVideo
+//
+//  Created by cis290 on 10/30/17.
+//  Copyright © 2017 Rock Valley College. All rights reserved.
+//
+
+import UIKit
+
+class videos: NSObject {
+
+}
